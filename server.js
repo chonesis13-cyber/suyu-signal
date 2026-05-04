aae4f0117030d1206b5645f64cc3de92fcf9a356
chonesis13-cyber <chonesis13@gmail.com>
2026-05-05 00:40:36 +0900

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ function getSignalData() {
   const aIsGreen  = elapsed < GREEN_SEC;
   const aResid    = aIsGreen ? GREEN_SEC - elapsed : CYCLE - elapsed;
   const bIsGreen  = !aIsGreen;
-  const bResid    = bIsGreen ? (CYCLE - elapsed) : (GREEN_SEC - elapsed);
+  const bResid    = Math.max(1, bIsGreen ? (CYCLE - elapsed) : (GREEN_SEC - elapsed));
   return [
     {
       ITRSC_NM: CROSSINGS[0].name,
